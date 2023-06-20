8075b17fba09ffb51ad00b31b6909c464a954a03
Greg <greg.pounds3@gmail.com>
2023-06-20 03:21:50 -0500

    restart

--- client/components/Portfolio.js
+++ client/components/Portfolio.js
@@ -4,6 +4,9 @@ const Portfolio = ()=>{
   return(
 <div id="Portfolio">
 <h1>Portfolio</h1>
+<h1 className="text-3xl text-red-500 font-bold underline">
+    Hello world!
+  </h1>
 </div>
 )
 }
--- client/index.js
+++ client/index.js
@@ -1,4 +1,4 @@
-import './input.css';
+import '../public/input.css';
 import React from 'react';
 import { createRoot } from "react-dom/client";
 import { Provider } from 'react-redux';
